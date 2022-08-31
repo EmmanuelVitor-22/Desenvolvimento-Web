f1b37f7a21f46dd99710277baaac502f50e9afbd
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2022-08-31 13:11:27 -0300

    Questão 29 da lista de esxercicios

---
+++ Fundamentos JavaScript/estrutura-de-controle/exercicios/exec29.js
@@ -0,0 +1,10 @@
+// Objetivo IF e ELSE ... mas irei praticar com OBJETO e ARRAY
+let valorSalario = parseFloat(prompt("Diga seu salario:"))
+
+let descontoSalario = valorSalario * 0.11;
+
+if (descontoSalario >= 334.29) {
+    descontoSalario = 334.39;
+}
+
+console.log(descontoSalario);
