cff0aa623e6282300fdf5b1776c528da2c29c054
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2022-11-02 14:29:17 -0300

    feat: cria exemplo de metodo concat

---
+++ Fundamentos JavaScript/Array/6-concat.js
@@ -0,0 +1,11 @@
+// metodo usado para concatenar arrays, pode-se passar outros elementos
+
+
+
+const filhos = ["jons", "userkin"];
+const filhas = ["jiria", "albera"];
+
+const todosFilhos = filhos.concat(filhas); // gera um novo array resultante sem alterar os originais
+console.log(todosFilhos)
+
+console.log([].concat([1, 2], [0, 01], 21, "a", [["luftal"]]))
