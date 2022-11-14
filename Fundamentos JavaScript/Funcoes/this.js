f5e370ebc350539b3dea0062cfeeaa3425f8abc5
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2022-11-13 22:32:16 -0300

    feat:  cria explicação de como funciona o this em JAVASCRIPT e como ele pode ser variavel, além de explicar um pouco da função bind. ps: Existe uma aula 2 sobre this no bind, mas não escrevi sobre

---
+++ Fundamentos JavaScript/Funcoes/this.js
@@ -0,0 +1,26 @@
+// This é variavel ao escopo no JavaScript
+// This é lexico ou seja
+
+// ele vai ser criado no momemento que aquela função for criada
+// vai pertencer a esse escopo a qual a função foi definida
+// Quando existem função normal o this varia (de acordo com quem chama a função)
+// Em Arrow functions o  THIS é fixo
+
+
+// THIS em função bind
+const obj = {
+    saudacao: 'Bom dia',
+    falar() {
+        console.log(this.saudacao); //referencia o atributo(este atributo) desse objeto seria o mesmo q fazer obj.saudacao
+    }
+}
+obj.falar();
+
+//  Função bind é utilizada para passar um objeto que se deseja ser "resolvido" o this
+// utilizado para "amarrar" um determinado objeto, dono da execução de um metodo expecifico
+//QUando passar para uma deteminada função, sempre que for referenciado o this, ele será o que foi passado no bind
+// ex:
+const falarBomDia = obj.falar;
+falarBomDia()
+const darBomDia = obj.falar.bind(obj);
+darBomDia(); //Bind gera uma nova função
